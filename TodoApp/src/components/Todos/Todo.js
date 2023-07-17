64e7524e385a77cc8b18c4cea62db055b8524487
AnB801 <antony.bondarev@gmail.com>
2023-07-17 12:44:55 +0200

    adding two buttons - dellete task and complete task

--- TodoApp/src/components/Todos/Todo.js
+++ TodoApp/src/components/Todos/Todo.js
@@ -1,13 +1,26 @@
-import { RiBriefcase3Fill } from 'react-icons/ri'
+import { RiBriefcase3Fill, RiDeleteBin2Line } from 'react-icons/ri'
+import { FaCheck } from 'react-icons/fa'
 import styles from './Todo.module.css'
 
-function Todo({ todo, index, deleteTodo }) {
+function Todo({ todo, deleteTodo, toggleTodo }) {
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+    <div className={styles.todo}>
       {/* adding icons */}
-      <RiBriefcase3Fill className={styles.todoIcon} />
+      <RiBriefcase3Fill
+        className={`${styles.todo} ${
+          todo.isCompleted ? styles.completedTodo : ''
+        }`}
+      />
       {/* adding text of task */}
-      <div className={styles.todoText}>{todo}</div>
+      <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line
+        className={styles.deleteIcon}
+        onClick={() => deleteTodo(todo.id)}
+      />
+      <FaCheck
+        className={styles.checkIcon}
+        onClick={() => toggleTodo(todo.id)}
+      />
     </div>
   )
 }
